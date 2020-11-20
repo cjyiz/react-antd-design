ea6927272280f05d5f9476cf02ede8ef87839445
chenji <cjyiz@aliyun.com>
2020-11-21 01:04:45 +0800

    feat: button rewrite

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,63 +1,57 @@
-import React, { HtmlHTMLAttributes } from "react";
-import classNames from "classnames";
+import React from 'react'
+import classNames from 'classnames'
+// step1   创建枚举类型
 export enum ButtonSize {
-  Large = "lg",
-  Small = "sm",
+  Large = 'lg',
+  Small = 'sm'
 }
-
 export enum ButtonType {
-  Primary = "primary",
-  Default = "default",
-  Danger = "danger",
-  Link = "link",
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
 }
 
+// step2  创建接口
+// 创建的时候需要考虑可能会用到那些属性
 interface BaseButtonProps {
-  className?: string;
-  disabled?: boolean;
-  size?: ButtonSize;
-  btnType?: ButtonType;
+  className?: string,
+  disabled?: boolean,
+  size?: ButtonSize,
+  btnType?: ButtonType,
+  // children为什么是一个ReactNode?
   children: React.ReactNode;
-  href?: string;
+  href?:string
 }
-// 使用交叉类型 intersection  ，多种类型合并为一个新类型
-type NativeButtonProps = BaseButtonProps &
-  React.ButtonHTMLAttributes<HTMLElement>;
-type AnchorButtonProps = BaseButtonProps &
-  React.AnchorHTMLAttributes<HTMLElement>;
-  // partieal 将属性设置为可选,这样ButtonProps属性就是可选的了,因为button其中也包含了a标签，所以这两者都要考虑
-export type ButtonProps=Partial<NativeButtonProps &AnchorButtonProps>
-const Button: React.FC<ButtonProps> = (props) => {
-  // 这里使用...restProps 解构赋值其他所有属性
-  const { btnType,className, disabled, size, children, href,...restProps } = props;
-  const classes = classNames("btn",className, {
-    [`btn-${btnType}`]: btnType,
-    [`btn-${size}`]: size,
-    disabled: btnType === ButtonType.Link && disabled,
-  });
-  if (btnType === ButtonType.Link && href) {
-    return (
-      <a 
 
-      className={classes} href={href}
-      {...restProps}
-      >
+
+export const Button: React.FC<BaseButtonProps> = (props) => {
+  // 因为是组件，所以必然是外面的父元素传值给他
+  const { btnType, disabled, size, children,href } = props
+  // 使用classes管理类名
+  const classes=classNames('btn',{
+    // 这里的写法是:后面的变量如果存在，那么前面的类名就生效
+    [`btn-${btnType}`]:btnType,
+    [`btn-${size}`]:size,
+    'disabled':(btnType===ButtonType.Link)&&disabled
+  })
+  if(btnType===ButtonType.Link&&href){
+    return (
+      <a href={href} className={classes}>
         {children}
       </a>
-    );
-  } else {
+    )
+  }else{
     return (
-      <button className={classes} disabled={disabled}
-      {...restProps}
-      >
+      <button className={classes} disabled={disabled}>
         {children}
       </button>
-    );
+    )
   }
-};
+}
 
-Button.defaultProps = {
-  disabled: false,
-  btnType: ButtonType.Default,
-};
-export default Button;
+// 这个defaultProps也是React.FC自带属性
+Button.defaultProps={
+  disabled:false,
+  btnType:ButtonType.Default
+}
