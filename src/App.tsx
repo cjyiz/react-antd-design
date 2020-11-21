e6cf68fd8fe6d8a10a3a3a473c95b44f9e145ee6
chenji <cjyiz@aliyun.com>
2020-11-21 15:50:21 +0800

    fix: add note

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import {Button} from '../src/components/Button/button'
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Button></Button>
       </header>
     </div>
   );
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -21,29 +21,35 @@ interface BaseButtonProps {
   btnType?: ButtonType,
   // children为什么是一个ReactNode?
   children: React.ReactNode;
-  href?:string
+  href?: string
 }
-
-
-export const Button: React.FC<BaseButtonProps> = (props) => {
-  // 因为是组件，所以必然是外面的父元素传值给他
-  const { btnType, disabled, size, children,href } = props
+// 使用本身的类型约束覆盖全部默认的props,单独的一个&类似于mixin作用，即合并两个属性
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+// 这里partial修饰符是使得上面两个类型中的所有参数都变得可选
+export type ButtonProps=Partial<NativeButtonProps & AnchorButtonProps>
+export const Button: React.FC<ButtonProps> = (props) => {
+  // 因为是组件，所以必然是外面的父元素传值给他。
+  // add1:因为上面替换了新的buttonProps，存在一些默认的props，所以用ES6的解构语法解析出来...restProps(这个名字可自取)
+  // add2:props中自定义className，可以直接取出来放到classNames中
+  const { btnType,className, disabled, size, children, href ,...restProps} = props
   // 使用classes管理类名
-  const classes=classNames('btn',{
+  const classes = classNames('btn', className,{
     // 这里的写法是:后面的变量如果存在，那么前面的类名就生效
-    [`btn-${btnType}`]:btnType,
-    [`btn-${size}`]:size,
-    'disabled':(btnType===ButtonType.Link)&&disabled
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === ButtonType.Link) && disabled
   })
-  if(btnType===ButtonType.Link&&href){
+  // 可以把...restProps的参数解构到html标签中
+  if (btnType === ButtonType.Link && href) {
     return (
-      <a href={href} className={classes}>
+      <a href={href} className={classes} {...restProps}>
         {children}
       </a>
     )
-  }else{
+  } else {
     return (
-      <button className={classes} disabled={disabled}>
+      <button className={classes} disabled={disabled} {...restProps}>
         {children}
       </button>
     )
@@ -51,7 +57,7 @@ export const Button: React.FC<BaseButtonProps> = (props) => {
 }
 
 // 这个defaultProps也是React.FC自带属性
-Button.defaultProps={
-  disabled:false,
-  btnType:ButtonType.Default
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
 }
